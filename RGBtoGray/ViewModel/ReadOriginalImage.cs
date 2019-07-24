54ce6bc7b7ad98cce0011887fc5ca85e74a811a4
Andrzej Krupa <andrzej.jaroslaw.krupa@gmail.com>
2019-07-24 15:44:59 +0200

    Fixed Filename property

--- RGBtoGray/ViewModel/ReadOriginalImage.cs
+++ RGBtoGray/ViewModel/ReadOriginalImage.cs
@@ -5,89 +5,90 @@ using System.Windows.Media.Imaging;
 
 namespace RGBtoGray.ViewModel
 {
-    class ReadOriginalImage : ObservableObject
-    {
-        private string _filename;
-        private string _selectedPath;
-        private BitmapImage _originalImage;
+	class ReadOriginalImage : ObservableObject
+	{
+		private string _filename;
+		private string _selectedPath;
+		private BitmapImage _originalImage;
 
-        public string Filename
-        {
-            get => _filename;
-            set
-            {
-                _filename = value;
-                RaisePropertyChangedEvent("Filename");
-            }
-        }
+		public string Filename
+		{
+			get => _filename;
+			set
+			{
+				_filename = value;
+				RaisePropertyChangedEvent("Filename");
+			}
+		}
 
-        public string SelectedPath
-        {
-            get => _selectedPath;
-            set
-            {
-                _selectedPath = value;
-                RaisePropertyChangedEvent("SelectedPath");
-            }
-        }
+		public string SelectedPath
+		{
+			get => _selectedPath;
+			set
+			{
+				_selectedPath = value;
+				RaisePropertyChangedEvent("SelectedPath");
+			}
+		}
 
-        public BitmapImage OriginalImage
-        {
-            get => _originalImage;
-            set
-            {
-                _originalImage = value;
-                RaisePropertyChangedEvent("OriginalImage");
-            }
-        }
-        
-        public ICommand OpenFileDialogCommand => new DelegateCommand(OpenFileDialog);
+		public BitmapImage OriginalImage
+		{
+			get => _originalImage;
+			set
+			{
+				_originalImage = value;
+				RaisePropertyChangedEvent("OriginalImage");
+			}
+		}
 
-        private void OpenFileDialog()
-        {
-            var fileDialog = new OpenFileDialog
-            {
-                DefaultExt = ".jpg",
-                Filter =
-                    "JPG Files (*.jpg)|*.jpg|JPEG Files (*.jpeg)|*.jpeg|PNG Files (*.png)|*.png|BMP Files (*.bmp)|*.bmp",
-                Title = "Please select an image file to convert.",
-                RestoreDirectory = true
-            };
-            var result = fileDialog.ShowDialog();
+		public ICommand OpenFileDialogCommand => new DelegateCommand(OpenFileDialog);
 
-            if (result == true)
-            {
-                SelectedPath = fileDialog.FileName;
-                ChangeFilenameFromPath(SelectedPath);
-                ChangeImageFromPath(SelectedPath);
-            }
-            else SelectedPath = null;
-        }
+		private void OpenFileDialog()
+		{
+			var fileDialog = new OpenFileDialog
+			{
+				DefaultExt = ".jpg",
+				Filter =
+					"JPG Files (*.jpg)|*.jpg|JPEG Files (*.jpeg)|*.jpeg|PNG Files (*.png)|*.png|BMP Files (*.bmp)|*.bmp",
+				Title = "Please select an image file to convert.",
+				RestoreDirectory = true
+			};
+			var result = fileDialog.ShowDialog();
 
-        private void ChangeFilenameFromPath(string path)
-        {
-            if (path == null) return;
-            try
-            {
-                var directorySplit = path.Split('\\');
-                Filename = directorySplit[directorySplit.Length - 1];
-            }
-            catch (Exception e)
-            {
-                throw new ApplicationException("ChangeFilename: ", e);
-            }
-        }
+			if (result == true)
+			{
+				SelectedPath = fileDialog.FileName;
+				ChangeFilenameFromPath(SelectedPath);
+				ChangeImageFromPath(SelectedPath);
+			}
+			else
+				SelectedPath = null;
+		}
 
-        private void ChangeImageFromPath(string path)
-        {
-            try
-            {
-                OriginalImage = new BitmapImage(new Uri(path));
-            }
-            catch(Exception e)
-            {
-                throw new ApplicationException("ChangeImage: ", e);
-            }
-        }
-    }
+		private void ChangeFilenameFromPath(string path)
+		{
+			if (path == null) return;
+			try
+			{
+				var directorySplit = path.Split('\\');
+				_filename = directorySplit[directorySplit.Length - 1];
+			}
+			catch (Exception e)
+			{
+				throw new ApplicationException("ChangeFilename: ", e);
+			}
+		}
+
+		private void ChangeImageFromPath(string path)
+		{
+			try
+			{
+				OriginalImage = new BitmapImage(new Uri(path));
+			}
+			catch (Exception e)
+			{
+				throw new ApplicationException("ChangeImage: ", e);
+			}
+		}
+	}
 }
