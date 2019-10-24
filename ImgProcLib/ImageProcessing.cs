e33f56099eeafea42bddde6b39828ace071759ef
Andrzej Krupa <andrzej.jaroslaw.krupa@gmail.com>
2019-10-24 16:25:39 +0200

    Implemented grayscale algorithm

--- ImgProcLib/ImageProcessing.cs
+++ ImgProcLib/ImageProcessing.cs
@@ -1,13 +1,23 @@
 ﻿using System;
+using System.IO;
+using System.Windows.Controls;
 using System.Windows.Media.Imaging;
 
 namespace ImgProcLib
 {
 	public class ImageProcessing
 	{
-		public BitmapImage ConvertToGrayscale(BitmapImage originalImage)
+		private const double R = 0.2126;
+		private const double G = 0.7152;
+		private const double B = 0.0722;
+
+		public BitmapImage ConvertBitmapImageToGrayscale(BitmapImage bitmapImage)
 		{
-			return originalImage;
+			var pixels = GetBitmapPixels(bitmapImage);
+
+			pixels = ConvertPixelsToGrayscale(pixels);
+
+			return CreateImageFile(bitmapImage, pixels);
 		}
 
 		public byte[] GetBitmapPixels(BitmapImage bitmapImage)
@@ -15,12 +25,50 @@ namespace ImgProcLib
 			if (bitmapImage == null)
 				throw new ArgumentNullException();
 
-			int stride = bitmapImage.PixelWidth * (bitmapImage.Format.BitsPerPixel / 8);
+			int stride = DetermineStride(bitmapImage);
 			byte[] pixels = new byte[bitmapImage.PixelHeight * stride];
 
 			bitmapImage.CopyPixels(pixels, stride, 0);
 
 			return pixels;
+		}
+
+		private byte[] ConvertPixelsToGrayscale(byte[] pixels)
+		{
+			for (int y = 0; y < pixels.Length; y = y + 4)
+			{
+				pixels[y] = (byte)(R * pixels[y] + G * pixels[y + 1] + B * pixels[y + 2]);
+				pixels[y + 1] = pixels[y];
+				pixels[y + 2] = pixels[y];
+			}
+
+			return pixels;
+		}
+
+		private BitmapImage CreateImageFile(BitmapImage bitmapImage, byte[] pixels)
+		{
+			var stride = DetermineStride(bitmapImage);
+
+			var bitmapSource = BitmapSource.Create(bitmapImage.PixelWidth, bitmapImage.PixelHeight, bitmapImage.DpiX, bitmapImage.DpiY, bitmapImage.Format, bitmapImage.Palette, pixels, stride);
+
+			var encoder = new JpegBitmapEncoder();
+
+			encoder.Frames.Add(BitmapFrame.Create(bitmapSource));
+			var memoryStream = new MemoryStream();
+			encoder.Save(memoryStream);
+
+			memoryStream.Position = 0;
+			var convertedBitmapImage = new BitmapImage();
+			convertedBitmapImage.BeginInit();
+			convertedBitmapImage.StreamSource = memoryStream;
+			convertedBitmapImage.EndInit();
+
+			return convertedBitmapImage;
+		}
+
+		private int DetermineStride(BitmapImage bitmapImage)
+		{
+			return bitmapImage.PixelWidth * (bitmapImage.Format.BitsPerPixel / 8);
 		}
 	}
 }
--- ImgProcLibTests/ImageProcessingTests.cs
+++ ImgProcLibTests/ImageProcessingTests.cs
@@ -8,8 +8,10 @@ namespace ImgProcLibTests
 	[TestFixture]
 	public class ImageProcessingTests
 	{
-		private string _TestFilesDirectory = TestContext.CurrentContext.TestDirectory + @"\\TestFiles\\testImage.jpg";
+		private string _TestFilesDirectory = TestContext.CurrentContext.TestDirectory + @"\\TestFiles";
 		private ImageProcessing _ImageProcessing;
+		private readonly string _blackTestImage = @"\\blackTestImage.jpg";
+		private readonly string _redTestImage = @"\\redTestImage.jpg";
 
 		[SetUp]
 		public void Setup()
@@ -20,18 +22,40 @@ namespace ImgProcLibTests
 		[Test]
 		public void ConvertToGrayscale_BlackImage_ImageUnchanged()
 		{
-			var originalImage = new BitmapImage(new Uri(_TestFilesDirectory));
+			var originalImage = new BitmapImage(new Uri(_TestFilesDirectory + _blackTestImage));
 
-			var convertedImage = _ImageProcessing.ConvertToGrayscale(originalImage);
+			var convertedImage = _ImageProcessing.ConvertBitmapImageToGrayscale(originalImage);
 
 			CollectionAssert.AreEqual(_ImageProcessing.GetBitmapPixels(convertedImage), _ImageProcessing.GetBitmapPixels(originalImage));
 		}
 
 		[Test]
+		public void ConvertToGrayscale_RedImage_ImageConvertedCorectly()
+		{
+			var originalImage = new BitmapImage(new Uri(_TestFilesDirectory + _redTestImage));
+			var expected = new byte[] { 44, 44, 44, 255 };
+
+			var convertedImage = _ImageProcessing.ConvertBitmapImageToGrayscale(originalImage);
+
+			CollectionAssert.AreEqual(expected, _ImageProcessing.GetBitmapPixels(convertedImage));
+		}
+
+		[Test]
 		public void GetBitmapPixels_BlackImage_ValidByteArray()
 		{
-			var originalImage = new BitmapImage(new Uri(_TestFilesDirectory));
+			var originalImage = new BitmapImage(new Uri(_TestFilesDirectory + _blackTestImage));
 			var expected = new byte[] { 0, 0, 0, 255 };
+
+			var pixels = _ImageProcessing.GetBitmapPixels(originalImage);
+
+			CollectionAssert.AreEqual(expected, pixels);
+		}
+
+		[Test]
+		public void GetBitmapPixels_RedImage_ValidByteArray()
+		{
+			var originalImage = new BitmapImage(new Uri(_TestFilesDirectory + _redTestImage));
+			var expected = new byte[] { 36, 27, 237, 255 };
 
 			var pixels = _ImageProcessing.GetBitmapPixels(originalImage);
 
