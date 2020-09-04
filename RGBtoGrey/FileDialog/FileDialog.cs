dfc6f898ed33b104b491e6ce86ee7dbefeabe02b
Andrzej Krupa <andrzej.jaroslaw.krupa@gmail.com>
2020-09-04 12:48:30 +0200

    Refactored FileDialog

--- RGBtoGrey/FileDialog/FileDialog.cs
+++ RGBtoGrey/FileDialog/FileDialog.cs
@@ -1,4 +1,9 @@
-﻿namespace RGBtoGrey.FileDialog
+﻿using RGBtoGrey.ViewModel.FileManagement;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace RGBtoGrey.FileDialog
 {
 	internal class FileDialog : IFileDialog
 	{
@@ -7,18 +12,41 @@
 		public FileDialog(Microsoft.Win32.FileDialog fileDialog)
 		{
 			_fileDialog = fileDialog;
+			InitializeFileDialog();
 		}
 
 		public string FilePath { get; private set; }
 
-		public bool? ShowDialog()
+		private static Dictionary<ImageFileFormats, string> FilterDictionary =>
+			((ImageFileFormats[])Enum.GetValues(typeof(ImageFileFormats))).ToDictionary(
+				imageFileFormat => imageFileFormat, FormatFilter);
+
+		private static string DefaultExtension => "." + FilterDictionary.Keys.First();
+
+		private void InitializeFileDialog()
 		{
-			_fileDialog.DefaultExt = ".jpg";
-			_fileDialog.Filter =
-				"JPG Files (*.jpg)|*.jpg|JPEG Files (*.jpeg)|*.jpeg|PNG Files (*.png)|*.png|BMP Files (*.bmp)|*.bmp";
+			_fileDialog.DefaultExt = DefaultExtension;
+			_fileDialog.Filter = GenerateFilter();
 			_fileDialog.Title = "Please select an image file to convert.";
 			_fileDialog.RestoreDirectory = true;
+		}
 
+		private static string GenerateFilter()
+		{
+			var filter = FilterDictionary.Aggregate(string.Empty, (current, fileFormat) => current + fileFormat.Value);
+
+			return filter.TrimEnd('|');
+		}
+
+		private static string FormatFilter(ImageFileFormats format)
+		{
+			var formatString = format.ToString();
+
+			return formatString.ToUpper() + " Files (*." + formatString + ")|*." + formatString + "|";
+		}
+
+		public bool? ShowDialog()
+		{
 			var didFileDialogOpen = _fileDialog.ShowDialog();
 
 			FilePath = didFileDialogOpen == true ? _fileDialog.FileName : "";
